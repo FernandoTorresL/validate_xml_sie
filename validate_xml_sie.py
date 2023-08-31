fb6860c7e1835348a8ded8579c30013e99020112
Fer Torres <fertorresmx@gmail.com>
2023-08-31 13:53:06 -0600

    Add output file and print parameters

--- validate_xml_sie.py
+++ validate_xml_sie.py
@@ -1,6 +1,7 @@
 # validate_xml_sie.py
 
 import argparse
+import datetime
 from configparser import ConfigParser
 
 
@@ -19,13 +20,13 @@ def read_user_cli_args():
     )
     parser.add_argument(
         "-x",
-        "--xsd",
+        "--xsd_check",
         action="store_true",
         help="check xml file vs xsd definition file",
     )
     parser.add_argument(
         "-r",
-        "--renapo",
+        "--renapo_check",
         action="store_true",
         help="check xml data vs WS RENAPO",
     )
@@ -45,7 +46,7 @@ def _get_ws_renapo_url():
     config.read("secrets.ini")
     return config["urls"]["ws_url_renapo"]
 
-def _get_xsd_filename():
+def _get_xsd_file():
     """Fetch the XSD definition file from your configuration file.
 
     Expects a xsd definition file named "xsd_file.xsd":
@@ -60,8 +61,21 @@ def _get_xsd_filename():
     config.read("secrets.ini")
     return config["files"]["xsd_file"]
 
-def create_report():
-    pass
+def create_report_file(input_xml_file, xsd_file, xsd_check, renapo_check):
+        now = datetime.datetime.now().strftime('%Y_%m_%d_%H%M%S')
+        output_report_file = './output_files/{datetime}_report.csv'.format(
+            datetime=now,input_xml_file=input_xml_file)
+
+        csv_header = '# Input XML File:' + input_xml_file
+        csv_header = csv_header + '|XSD File:' + xsd_file
+        csv_header = csv_header + '|xsd_check:' + str(xsd_check)
+        csv_header = csv_header + '|renapo_check:' + str(renapo_check)
+
+        with open(output_report_file, mode='w+', newline='', encoding='utf-8') as f:
+            f.write(csv_header+ "\n")
+
+        return output_report_file
+
 
 def read_xml():
     pass
@@ -92,14 +106,21 @@ if __name__ == '__main__':
 
     user_args = read_user_cli_args()
     # Check the args values
-    print(f"\tParameters:{user_args.xml_file, user_args.xsd, user_args.renapo}", end="\n\n")
+    print(f"\tParameters:{user_args.xml_file, user_args.xsd_check, user_args.renapo_check}", end="\n\n")
+    print(f"\tParameter xml_file:\t{user_args.xml_file}", end="\n")
+    print(f"\tParameter xsd_check:\t{user_args.xsd_check}", end="\n")
+    print(f"\tParameter renapo_check:\t{user_args.renapo_check}", end="\n\n")
 
     # Testing getting a secret
-    print(f"\tInput XML File:\t{user_args.xml_file}", end="\n")
+    input_xml_file = user_args.xml_file[0]
+    print(f"\tInput XML File:\t{input_xml_file}", end="\n")
 
-    ws_renapo_url=_get_ws_renapo_url()
+    ws_renapo_url = _get_ws_renapo_url()
     print(f"\tURL WS_RENAPO:\t{ws_renapo_url}", end="\n")
 
-    xsd_filename=_get_xsd_filename()
-    print(f"\tXSD File:\t{xsd_filename}", end="\n")
+    xsd_file = _get_xsd_file()
+    print(f"\tXSD File:\t{xsd_file}", end="\n\n")
+
+    output_file = create_report_file(input_xml_file, xsd_file, user_args.xsd_check, user_args.renapo_check)
+    print(f"\tOutput File:\t{output_file}", end="\n")
 

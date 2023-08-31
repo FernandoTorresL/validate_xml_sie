efce6487ff0b655f25037dc21ccbbe6a79fba58d
Fer Torres <fertorresmx@gmail.com>
2023-08-31 12:29:11 -0600

    Add user-provided info parameters

--- validate_xml_sie.py
+++ validate_xml_sie.py
@@ -1,6 +1,36 @@
 # validate_xml_sie.py
 
+import argparse
 from configparser import ConfigParser
+
+
+def read_user_cli_args():
+    """Handles the CLI user interactions.
+
+    Returns:
+        argparse.Namespace: Populated namespace object
+    """
+    parser = argparse.ArgumentParser(
+        description="validate xml file and xml tags vs a Web Service data"
+    )
+
+    parser.add_argument(
+        "xml_filename", nargs="+", type=str, help="enter the xml filename"
+    )
+    parser.add_argument(
+        "-x",
+        "--xsd",
+        action="store_true",
+        help="check xml file vs xsd definition file",
+    )
+    parser.add_argument(
+        "-r",
+        "--renapo",
+        action="store_true",
+        help="check xml data vs WS RENAPO",
+    )
+
+    return parser.parse_args()
 
 
 def _get_ws_renapo_url():
@@ -41,6 +71,13 @@ def save_on_report():
 
 
 if __name__ == '__main__':
+
+    # Get user parameters
+    read_user_cli_args()
+
+    user_args = read_user_cli_args()
+    # Check the args values
+    print(user_args.xml_filename, user_args.xsd, user_args.renapo)
 
     # Testing getting a secret
     print(_get_ws_renapo_url())

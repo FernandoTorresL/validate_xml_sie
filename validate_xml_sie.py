02b5d5e687fbd23fdc347b371493328e8b123e06
Fer Torres <fertorresmx@gmail.com>
2023-08-31 10:52:44 -0600

    Create general structure

---
+++ validate_xml_sie.py
@@ -0,0 +1,28 @@
+
+def create_report():
+    pass
+
+def read_xml():
+    pass
+
+def validate_vs_xsd():
+    pass
+
+def analyzing_data():
+    pass
+
+def validate_xsd_regex():
+    pass
+
+def validate_custom_rules():
+    pass
+
+def search_on_ws():
+    pass
+
+def save_on_report():
+    pass
+
+
+if __name__ == '__main__':
+    pass
